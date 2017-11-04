deccbce50e7cb1b53d691b0ff4a1fe90380edd82
Jetroid <holtjet@gmail.com>
2017-11-04 16:12:35 +0000

    Add CORS header

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var words = require('./words');
+
+var allowedOrigins = ['http://localhost',
+                      'https://jetholt.com'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.set('port', (process.env.PORT || 4888));
 
